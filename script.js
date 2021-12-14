8337f4ac4d06e864b1f56e7a3e2fc79dbc611de8
caroline.cantaluppi <caroline.cantaluppi@gmail.com>
2021-12-14 21:44:06 +0100

    inserzione card

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //inserendo scroll in navbar:
-document.addEventListener('scroll', () => {
-	let scrolled = window.scrollY;
-	if(scrolled >= 20){
-        // quando scrolla, rimuove il colore iniziale della navbar:
-        navbarId.classList.remove('bg-light', 'navbar-light');
-        // quando scrolla, aggiunge il colore transparente nella navbar: 
-        navbarId.classList.add('bg-transparent');
-        // quando scrolla, ingrandisce la navbar aumentando l’altezza in 150px:
-		navbarId.style.height = '50px';
-	} else {
-        navbarId.classList.remove('bg-transparent');
-        navbarId.classList.add('bg-light', 'navbar-light');
-		navbarId.style.height = '50px';
-	}
-})
+// let navbarId = document.querySelector('#navbarId');
+// document.addEventListener('scroll', () => {
+// 	let scrolled = window.scrollY;
+// 	if(scrolled >= 20){
+//         // quando scrolla, rimuove il colore iniziale della navbar:
+//         navbarId.classList.remove('bg-light', 'navbar-light');
+//         // quando scrolla, aggiunge il colore transparente nella navbar: 
+//         navbarId.classList.add('bg-transparent');
+//         // quando scrolla, ingrandisce la navbar aumentando l’altezza in 150px:
+// 		navbarId.style.height = '50px';
+// 	} else {
+//         navbarId.classList.remove('bg-transparent');
+//         navbarId.classList.add('bg-light', 'navbar-light');
+// 		navbarId.style.height = '50px';
+// 	}
+// })
+
+// inserendo Swiper -->
+let swiper = new Swiper(".mySwiper", {
+    watchSlidesProgress: true,
+    slidesPerView: 8,
+});
+    
